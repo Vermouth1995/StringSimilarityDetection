8ed35045a584e4c168b64fb2fb4cdc64f67a188d
vermouth1995 <sixpenny1995@gmail.com>
2018-08-14 17:44:28 +0800

    :construction: update

--- src/detect/detect.go
+++ src/detect/detect.go
@@ -1,11 +1,19 @@
 package detect
 
 func SimilarityDetect(a string, b string) float64 {
-	distance := EditDistanceRecursion(a, b, len(a), len(b))
+	distance := EditDistanceDynamic(a, b)
 	ratio := float64(distance) / float64(MaxLen(a, b))
 	return 1 - ratio
 }
 
+/**
+ * 递归算法
+ * @param {string} a
+ * @param {string} b
+ * @param {int} len_a 字符串 a 的长度
+ * @param {int} len_b 字符串 b 的长度
+ * @return {int} 从 a → b 的最小编辑距离
+ */
 func EditDistanceRecursion(a string, b string, len_a int, len_b int) int {
 	if len_a == 0 {
 		return len_b
@@ -20,6 +28,31 @@ func EditDistanceRecursion(a string, b string, len_a int, len_b int) int {
 	}
 }
 
+/**
+ * 动态规划算法
+ * @param {string} a
+ * @param {string} b
+ * @return {int} 从 a → b 的最小编辑距离
+ */
 func EditDistanceDynamic(a string, b string) int {
-	return 1
+	var (
+		len_a int = len(a)
+		len_b int = len(b)
+		dis [][]int
+		i int
+		j int
+	)
+
+	for (i = 0; i <= len_a; i++) {
+        dis[i][0] = i;
+    }
+    for (j = 0; j <= len_b; j++) {
+        dis[0][j] = j;
+    }
+
+	for i = 0; i < len_a; i++ {
+		// TODO
+	}
+
+	return dis[len_a][len_b]
 }
